9bdd5a423126df780292c897a11bfa5a1667b415
Aguiardavidm <Aguiardavidm@gmail.com>
2018-03-06 17:38:38 -0700

    First commit

---
+++ customColor.kt
@@ -0,0 +1,8 @@
+package com.example.android.number2
+
+data class customColor(var red:Int, var green:Int, var blue:Int, var name:String)
+{
+    override fun toString(){ String = "$name $red $green $blue" }
+
+}
+}
